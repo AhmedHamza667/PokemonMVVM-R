a5b5d9c84e6bb89b82b0150a8100ffab858b2a7c
AhmedHamza667 <hamzaa667788@gmail.com>
2025-03-12 18:37:39 -0400

    Initial Commit

---
+++ PokemonMVVMR/PokemonMVVMRApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonMVVMRApp.swift
+//  PokemonMVVMR
+//
+//  Created by Ahmed Hamza on 3/12/25.
+//
+
+import SwiftUI
+
+@main
+struct PokemonMVVMRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
